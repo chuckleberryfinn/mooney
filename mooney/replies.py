708d1d2628f17b7dfe0cfd613a0ffe4331a4a94a
nemo <nemo@redbrick.dcu.ie>
2018-08-11 18:12:58 +0100

    Restructured project

---
+++ mooney/replies.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from mooney.db import CoinsDatabase
+
+
+class Reply(object):
+    def __init__(self):
+        self.cdb = CoinsDatabase()
+
+
+class Advice(Reply):
+    def response(self):
+        return self.cdb.get_advice()[0]
+
+
+class Auto(Reply):
+    def response(self, comment):
+        c = self.cdb.get_remark(comment)
+        return c[0] if c else None
+
+
+class Targeted(Reply):
+    def __init__(self, sender):
+        super(Targeted, self).__init__()
+        self.sender = sender
+
+    def response(self, comment):
+        c = self.cdb.get_targeted_remark(self.sender, comment)
+        return c[0] if c else None
+
+
+class Admin(Reply):
+    def response(self, user):
+        c = self.cdb.is_admin(user)
+        return True if c else False
+
+
+class Date(Reply):
+    def response(self):
+        d = self.cdb.min_date()
+        return d[0]
+
+
+if __name__ == '__main__':
+    pass
---
+++ tests/test_triggers.py
@@ -0,0 +1,12 @@
+import unittest
+
+from mooney import triggers
+
+
+class TestTriggers(unittest.TestCase):
+    def test_coin(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
