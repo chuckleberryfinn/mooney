6a8d074eac24f9cab5aa72c4aa672992d2af3486
nemo <nemo@redbrick.dcu.ie>
2018-08-12 22:46:35 +0100

    Use IRC Event for mooney tests

--- tests/test_triggers.py
+++ tests/test_triggers.py
@@ -1,60 +1,53 @@
 import unittest
 
+from irc.client import Event, NickMask
+
 from mooney import triggers
-
-
-class TestMessage(object):
-
-    class Source(object):
-        def __init__(self, source='#channel', nick='test'):
-            self.nick = nick
-            self.user = nick
-            self.source = source
-
-    def __init__(self, msg):
-        self.arguments = [msg]
-        self.source = TestMessage.Source()
 
 
 class TestTriggers(unittest.TestCase):
     def test_advice(self):
-        self.assertEqual(triggers.Trigger.make(TestMessage('!advice')).message(), 'Don\'t buy bitcoin!')
+        self.assertEqual(triggers.Trigger.make(self.create_event('!advice')).message(), 'Don\'t date robots!')
 
     def test_coin(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!coin')).message().startswith(
+        self.assertTrue(triggers.Trigger.make(self.create_event('!coin')).message().startswith(
                         'Current Price for Bitcoin (BTC):'))
 
     def test_fiat(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!fiat')).message().startswith(
+        self.assertTrue(triggers.Trigger.make(self.create_event('!fiat')).message().startswith(
                          '1 Bitcoin (BTC) is worth'))
 
     def test_help(self):
-        self.assertEqual(triggers.Trigger.make(TestMessage('!help')).message(),
-                         'Commands: !advice !ats !bears !bulls !help !coin !diff !fiat !stats. !help [command] for more information on a specific command.')
+        self.assertEqual(triggers.Trigger.make(self.create_event('!help')).message(),
+                        ('Commands: !advice !ats !bears !bulls !help !coin !diff !fiat !stats. ' +
+                         '!help [command] for more information on a specific command.'))
 
     def test_help_advice(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help advice')).message().startswith('!advice'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help advice')).message().startswith('!advice'))
 
     def test_help_ats(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help ats')).message().startswith('!ats'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help ats')).message().startswith('!ats'))
 
     def test_help_bears(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help bears')).message().startswith('!bears'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help bears')).message().startswith('!bears'))
 
     def test_help_bulls(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help bulls')).message().startswith('!bulls'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help bulls')).message().startswith('!bulls'))
 
     def test_help_coin(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help coin')).message().startswith('!coin'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help coin')).message().startswith('!coin'))
 
     def test_help_diff(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help diff')).message().startswith('!diff'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help diff')).message().startswith('!diff'))
 
     def test_help_fiat(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help fiat')).message().startswith('!fiat'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help fiat')).message().startswith('!fiat'))
 
     def test_help_stats(self):
-        self.assertTrue(triggers.Trigger.make(TestMessage('!help stats')).message().startswith('!stats'))
+        self.assertTrue(triggers.Trigger.make(self.create_event('!help stats')).message().startswith('!stats'))
+
+    def create_event(self, msg):
+        return Event('privmsg', NickMask('user!username@example.com'), '#channel', [msg])
 
 
 if __name__ == '__main__':
