e281a3715fe35dd368ccd028c0018b5dd7b49cd1
nemo <nemo@redbrick.dcu.ie>
2018-08-13 19:53:22 +0100

    Add daily_stats values

--- tests/populate.sql
+++ tests/populate.sql
@@ -1,3 +1,18 @@
 insert into coins(name, ticker) values ('bitcoin', 'btc');
 insert into prices(coin_id, euro, dollar) values (1, 5452.41, 6334.79);
 insert into advice(response) values ('Don''t date robots!');
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-12',14351.652580645161,14438.67,14201.53,79.5035930601208917,14370.7800000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-13',14168.382580645161,14400.17,13614.46,   173.472240855006,14178.8500000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-14',13794.422160278746,   14077,13500.72,   126.640579174256,13810.7800000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-15',14651.812543554007,15102.56,13847.01,   301.861419466814,14808.5400000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-16',15466.514861111111,16508.23,14711.17,   568.600497806268, 15280.050000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-17',16268.873414634146,16688.44,15799.18,   206.605702175946,16233.9500000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-18',15874.140871080139,16172.78,15351.32,   154.291017540446,15889.6800000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-19',15561.608815331010,15999.01,14535.93,   414.372973540369,15525.2600000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-20',14302.262648083624,15248.47,13413.66,   396.019630977830,14235.3000000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-21',13693.296877192982,14680.62,12311.98,   628.839154259296,13978.0700000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-22',11550.757491289199,13172.62, 9541.06,   752.343694565583,11600.7500000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-23',12665.904529616725,13499.81,11709.21,   441.575977122643,12829.4600000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-24',11608.594703832753,12569.77,10647.00,   451.018067929798,11520.5500000000000000);
+insert into daily_stats(coin_id, date, average_euro, max_euro, min_euro, std_dev, median_euro) values(1,'2017-12-25',11819.046111111111,12339.08,11301.15,   208.649281136617,11822.2900000000000000);
+
